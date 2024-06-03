d18426751efc50366499408b3f5a4a48877c45c5
Thibaut833 <thibautrichard8@outlook.fr>
2024-06-03 23:23:25 +0200

    ch10 function ✍️: practical

---
+++ ch10_functions/main.go
@@ -0,0 +1,63 @@
+/*
+Refactor this code and create functions that will :
+
+	Compute the occupancy level
+
+	Compute the occupancy rate
+
+	Print the details of a specific room.
+*/
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	const hotelName = "Gopher Paris Inn"
+	const totalRooms = 134
+	const firstRoomNumber = 110
+
+	rand.Seed(time.Now().UTC().UnixNano())
+	roomsOccupied := rand.Intn(totalRooms)
+	roomsAvailable := totalRooms - roomsOccupied
+
+	occupancyRate := (float32(roomsOccupied) / float32(totalRooms)) * 100
+	var occupancyLevel string
+	if occupancyRate > 70 {
+		occupancyLevel = "High"
+	} else if occupancyRate > 20 {
+		occupancyLevel = "Medium"
+	} else {
+		occupancyLevel = "Low"
+	}
+
+	fmt.Println("Hotel:", hotelName)
+	fmt.Println("Number of rooms", totalRooms)
+	fmt.Println("Rooms available", roomsAvailable)
+	fmt.Println("                  Occupancy Level:", occupancyLevel)
+	fmt.Printf("                  Occupancy Rate: %0.2f %%\n", occupancyRate)
+
+	if roomsAvailable > 0 {
+		fmt.Println("Rooms:")
+		for i := 0; roomsAvailable > i; i++ {
+			roomNumber := firstRoomNumber + i
+			size := rand.Intn(6) + 1
+			nights := rand.Intn(10) + 1
+			fmt.Println(roomNumber, ":", size, "people /", nights, " nights ")
+		}
+	} else {
+		fmt.Println("No rooms available for tonight")
+	}
+
+}
+
+func occupancyLevel() {
+
+}
+
+func occupancyRate() {
+
+}
